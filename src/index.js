df63c2dd2bf5f09664e9fbaaaba763e85fdd3940
Tsubasa Namiki <namikingsoft@gmail.com>
2018-04-13 04:45:10 +0900

    WIP

--- src/index.js
+++ src/index.js
@@ -92,21 +92,65 @@ const mecabStdio = ({
   };
 };
 
-const mecab = mecabStdio({ nbest: 3 });
-mecab.parse('中居正広\n木村拓哉がおわします\n受容性').then(xs => {
-  console.log(xs);
-  mecab.exit();
-  const db = new Database('/resources/wnjpn.db');
-  console.log(
-    db
+// const mecab = mecabStdio({ nbest: 1 });
+// mecab.parse('似つかわしげ').then(xs => {
+//   console.log(xs);
+//   mecab.exit();
+// });
+const db = new Database('/resources/wnjpn.db');
+const senses = db
+  .prepare(
+    `
+    SELECT sense.synset, word.lemma FROM sense
+      INNER JOIN word ON word.wordid = sense.wordid
+      WHERE word.lemma LIKE ?
+      ORDER BY sense.synset ASC
+  `,
+  )
+  .all('%淫乱%');
+console.log(senses);
+
+senses.forEach(({ synset }) => {
+  const childSenses = db
+    .prepare(
+      `
+      SELECT sense.synset, word.lemma FROM sense
+        INNER JOIN word ON word.wordid = sense.wordid
+        WHERE sense.synset = ?
+          AND sense.lang = 'jpn'
+        ORDER BY sense.synset ASC
+    `,
+    )
+    .all(synset);
+  console.log(childSenses);
+});
+
+senses.forEach(({ synset }) => {
+  const synsSenses = db
+    .prepare(
+      `
+      SELECT synlink.link, synlink.synset2 FROM sense
+        INNER JOIN word ON word.wordid = sense.wordid
+        INNER JOIN synlink ON synlink.synset1 = sense.synset
+        WHERE sense.synset = ?
+          AND sense.lang = 'jpn'
+          -- AND synlink.link = 'syns'
+        ORDER BY sense.synset ASC
+    `,
+    )
+    .all(synset);
+  synsSenses.forEach(({ link, synset2 }) => {
+    const childSenses = db
       .prepare(
         `
         SELECT sense.synset, word.lemma FROM sense
           INNER JOIN word ON word.wordid = sense.wordid
-          WHERE word.lemma LIKE ?
+          WHERE sense.synset = ?
+            AND sense.lang = 'jpn'
           ORDER BY sense.synset ASC
       `,
       )
-      .all('%かわし%'),
-  );
+      .all(synset2);
+    if (childSenses.length > 0) console.log(link, childSenses);
+  });
 });
